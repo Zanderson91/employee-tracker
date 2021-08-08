793e5eb6fbfd168bff633c5d559c75d665e158fd
Zack Anderson <zack.anderson2011@gmail.com>
2021-08-08 17:24:25 -0500

    Creating seed information for roles within departments

--- seeds.sql
+++ seeds.sql
@@ -8,4 +8,22 @@ VALUE (Social Media Design);
 INSERT INTO department (name)
 VALUE (Visual Design);
 
+--INFO FOR EACH ROLE--
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lead Developer", 150000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Web Developer", 70000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lead Designer", 200000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Assistant Designer", 100000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Office Manager", 50000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Social Lead", 100000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Content Creator", 120000, 3);
+
+
+
 
