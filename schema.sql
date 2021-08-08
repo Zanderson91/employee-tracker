e250c43fb2afd1c2e18ef950357ad56e509d3ef3
Zack Anderson <zack.anderson2011@gmail.com>
2021-08-08 17:03:17 -0500

    Setting up DATABASE and working on department/role table

--- schema.sql
+++ schema.sql
@@ -9,3 +9,12 @@ CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
+
+--Role within department table
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30)
+    salary DECIMAL,
+    department_id INT,
+    FOREIGHN KEY (department_id) REFERENCES department(id)
+);
