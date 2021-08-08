4f0e8c5c46a939df7f08660ca03c33ccd872a9c0
Zack Anderson <zack.anderson2011@gmail.com>
2021-08-08 17:29:11 -0500

    WIP..testing manager id along with role id

--- seeds.sql
+++ seeds.sql
@@ -24,6 +24,20 @@ VALUE ("Social Lead", 100000, 3);
 INSERT INTO role (title, salary, department_id)
 VALUE ("Content Creator", 120000, 3);
 
-
+--INFO FOR EACH EMPLOYEE--
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Jeremy", "Cox", 2, 2);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Zack", "Anderson", null, 2);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Jared", "Hardwick", 4, 4);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Meghan", "Shortwick", null, 4);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Daniel", "Hannon", 1, 1);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Josh", "Cobos", 3, 3);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("John", "Cobos", null, 3);
 
 
