f7954993992098068b00e7cd49b3f980f216d539
Zack Anderson <zack.anderson2011@gmail.com>
2021-08-08 17:06:53 -0500

    Set up employee table and using role, manager, and department as foreign keys

--- schema.sql
+++ schema.sql
@@ -16,5 +16,16 @@ CREATE TABLE role (
     title VARCHAR(30)
     salary DECIMAL,
     department_id INT,
-    FOREIGHN KEY (department_id) REFERENCES department(id)
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+--Employee within department table
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    manager_id INT,
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
